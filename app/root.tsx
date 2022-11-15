702c507d37ad289b03d3927c670aefcdb66d8636
Viranchee Lotia <viranchee@outlook.com>
2022-11-14 23:56:10 -0500

    Basic site (#4)
    * Remove unnecessary line
    
    * Add color mode switcher
    
    * update packages
    
    * Change defaults
    
    * Use my data
    
    * add gitignore

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-// import type { MetaFunction } from "@remix-run/cloudflare";
 import React, { useContext, useEffect } from 'react'
 import { withEmotionCache } from '@emotion/react'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -10,26 +9,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
-import { MetaFunction, LinksFunction } from '@remix-run/cloudflare' // Depends on the runtime you choose
+import { MetaFunction } from '@remix-run/cloudflare'
 
 import { ServerStyleContext, ClientStyleContext } from './context'
+import { ColorModeSwitcher } from './colorModeSwitcher'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Viranchee',
   viewport: 'width=device-width,initial-scale=1',
 });
 
-// export let links: LinksFunction = () => {
-//   return [
-//     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-//     { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-//     {
-//       rel: 'stylesheet',
-//       href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
-//     },
-//   ]
-// }
 
 interface DocumentProps {
   children: React.ReactNode;
@@ -78,10 +68,12 @@ const Document = withEmotionCache(
   }
 );
 
+
 export default function App() {
   return (
     <Document>
       <ChakraProvider>
+        {/* <ColorModeSwitcher /> */}
         <Outlet />
       </ChakraProvider>
     </Document>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,44 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { Icon, VStack } from '@chakra-ui/react';
 import { Box, Heading, HStack, Link as ChakraLink } from '@chakra-ui/react';
 import { Link } from "@remix-run/react";
 
+
+const linkStack = <HStack spacing={4}>
+  <ChakraLink
+    href="https://www.github.com/viranchee"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <Icon as={AiFillGithub} w={12} h={12} />
+  </ChakraLink>
+  <ChakraLink
+    target="_blank"
+    href="https://linkedin.com/in/viranchee"
+    rel="noreferrer"
+  >
+    <Icon as={AiFillLinkedin} w={12} h={12} />
+  </ChakraLink>
+</HStack>;
+
+const description = <VStack>
+  <Heading as="h3" size="xl" fontFamily={"Helvetica"} fontWeight="hairline">
+    Viranchee Lotia
+  </Heading >
+  <Heading as="h3" size="xl" fontFamily={"Helvetica"} fontWeight="hairline">
+    Compiler Engineer | Mobile Engineer
+  </Heading>
+  {linkStack}
+</VStack>
+
 export default function Index() {
   return (
-    <Box fontFamily="system-ui, sans-serif" lineHeight={1.4}>
-      <Heading as="h1" color="blue.400">
-        Welcome to Remix with Chakra UI!
-      </Heading>
-      <HStack spacing={4}>
-        <ChakraLink
-          href="https://remix.run/docs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Remix Docs
-        </ChakraLink>
-        <ChakraLink
-          target="_blank"
-          href="https://chakra-ui.com/docs/getting-started"
-          rel="noreferrer"
-        >
-          Chakra UI Docs
-        </ChakraLink>
-
-        <ChakraLink as={Link} color="red.400" to="/nothing-here">
-          Nothing here
-        </ChakraLink>
-      </HStack>
-      <Box w='100%' h='200px' bgGradient='linear(to-l, #7928CA, #FF0080)' />
+    <Box fontFamily="Helvetica" lineHeight={1.4} fontWeight="hairline">
+      <Box w='100vw' h='100vh' bgGradient='linear(to-l, #7928CA, #FF0080)' opacity={50}>
+        <Box w='100%' h='100%' display='flex' justifyContent='right' alignItems='center'>
+          {description}
+        </Box>
+      </Box>
     </Box>
   );
 }
