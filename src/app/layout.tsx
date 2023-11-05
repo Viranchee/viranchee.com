92dfb90096c8a9e740304ee8512e196ff0924cce
Viranchee Lotia <viranchee@outlook.com>
2023-11-04 21:28:35 -0400

    Remove links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,16 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-const APP_NAME = "Viranchee's Personal Website";
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#FFFFFF",
 };
 
+const APP_NAME = "Hi, I'm Viranchee";
 export const metadata: Metadata = {
-  title: { default: APP_NAME, template: "%s | nextarter-chakra" },
-  description: "Next.js + chakra-ui + TypeScript template",
+  title: { default: APP_NAME, template: "%s | LOLOLOL" },
+  description: "Viranchee's portfolio",
   applicationName: APP_NAME,
   appleWebApp: {
     capable: true,
@@ -25,19 +25,6 @@ export const metadata: Metadata = {
   },
   formatDetection: {
     telephone: false,
-  },
-  openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
-    images: {
-      url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-      alt: "nextarter-chakra.sznm.dev og-image",
-    },
-  },
-  twitter: {
-    creator: "@sozonome",
-    card: "summary_large_image",
   },
 };
 
