8c078eee1bc6c8270bc7d6e03f9471eb0745b17b
Viranchee Lotia <viranchee@outlook.com>
2023-11-05 03:30:53 -0500

    Revert "Add the whole library folder"
    This reverts commit 5852a116a0d741e43d57721a66efcb2e84d0b50e.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,4 @@
 import { Providers } from "./providers";
-import type { Metadata } from "next";
-
-const APP_NAME = "viranchee";
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  themeColor: "#FFFFFF",
-};
-
-export const metadata: Metadata = {
-  title: { default: APP_NAME, template: "%s | nextarter-chakra" },
-  description: "Next.js + chakra-ui + TypeScript template",
-  applicationName: APP_NAME,
-};
 
 export default function RootLayout({
   children,
