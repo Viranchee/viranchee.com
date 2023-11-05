d89c285e22ab58392ca662effa379ed45e53dee1
Viranchee Lotia <viranchee@outlook.com>
2023-11-05 18:20:58 -0500

    Use turbo

--- src/lib/styles/theme/fonts.ts
+++ src/lib/styles/theme/fonts.ts
@@ -1,12 +1,11 @@
 import type { DeepPartial, Theme } from '@chakra-ui/react';
-import { Figtree as FontBody } from 'next/font/google';
 
-export const fontBody = FontBody({
-  subsets: ['latin'],
-  variable: '--font-body',
-});
+// export const fontBody = FontBody({
+//   subsets: ['latin'],
+//   variable: '--font-body',
+// });
 
 export const fonts: DeepPartial<Theme['fonts']> = {
-  heading: fontBody.style.fontFamily,
-  body: fontBody.style.fontFamily,
+  // heading: fontBody.style.fontFamily,
+  // body: fontBody.style.fontFamily,
 };
