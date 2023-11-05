457ef167e90e60ff1341d774f3197c2b91ae21f1
Viranchee Lotia <viranchee@outlook.com>
2023-11-05 03:47:25 -0500

    Typesafe apis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
+import { Metadata, Viewport } from "next";
 import { Providers } from "./providers";
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#FFFFFF",
+};
+const APP_NAME = "nextarter-chakra";
+
+export const metadata: Metadata = {
+  title: { default: APP_NAME, template: "%s | nextarter-chakra" },
+  description: "Next.js + chakra-ui + TypeScript template",
+  applicationName: APP_NAME,
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,10 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+export function Providers({ children }: ProvidersProps) {
   return (
     <CacheProvider>
       <ChakraProvider>{children}</ChakraProvider>
