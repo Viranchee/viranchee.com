20dd8f066b38c3cf6cb86a219900af4da575d0c0
Viranchee Lotia <hey@viranchee.com>
2024-06-25 05:11:33 -0400

    Remove output=server in astro-config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,15 @@
 /// <reference types="astro/client" />
 
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
-// type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+  // replace `MY_KV` with your KV namespace
+  MY_KV: KVNamespace;
+};
 
-// type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
-// type ENV = {
-//   // replace `MY_KV` with your KV namespace
-//   MY_KV: KVNamespace;
-// };
-
-// // use a default runtime configuration (advanced mode).
-// type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
-// declare namespace App {
-//   interface Locals extends Runtime {}
-// }
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
