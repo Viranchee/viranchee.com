1f37e6b306f396b9b03b42e501de3f988918a150
Viranchee Lotia <hey@viranchee.com>
2024-07-09 01:17:45 -0400

    Static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: 'https://www.viranchee.com',
   integrations: [mdx(), sitemap()],
-  output: "hybrid",
+  output: "static",
   adapter: cloudflare({
     platformProxy: {
       enabled: true
