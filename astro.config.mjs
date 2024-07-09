dc19c466671edf91351e8f7206d19c1564c292c0
Viranchee Lotia <hey@viranchee.com>
2024-07-09 02:10:12 -0400

    Fix Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.viranchee.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), react(), tailwind({applyBaseStyles: false,})],
   output: "hybrid",
   adapter: cloudflare({
     platformProxy: {
