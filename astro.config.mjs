b48808cbce497225721e501dfc0abe61ac34c37c
Viranchee Lotia <hey@viranchee.com>
2024-07-09 01:57:42 -0400

    Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.viranchee.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   output: "hybrid",
   adapter: cloudflare({
     platformProxy: {
